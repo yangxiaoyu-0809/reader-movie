4c968fa28b08f9d1ca10120291836eb31cba2b42
yangxiaoyu-0809 <1150055925@qq.com>
2017-10-11 16:08:30 +0800

    first commit

---
+++ pages/posts/post.js
@@ -0,0 +1,26 @@
+var postsData = require('../../data/posts-data.js')
+
+Page({
+  data: {
+
+  },
+  onLoad: function () {
+    this.setData({
+       postList:postsData.postList
+      });
+  },
+
+  onPostTap: function (event) {
+    var postId = event.currentTarget.dataset.postid;
+    wx.navigateTo({
+      url: "post-detail/post-detail?id=" + postId
+    })
+  },
+
+  onSwiperTap: function (event) {
+    var postId = event.target.dataset.postid;
+    wx.navigateTo({
+      url: "post-detail/post-detail?id=" + postId
+    })
+  }
+})
